fa67191587e63eea7e5b57df9150cc052be45272
Narwhal8 <narwhal09@icloud.com>
2024-08-15 23:55:50 -0400

    update to Crafting Claim Menu

--- address.js
+++ address.js
@@ -1,11 +1,11 @@
 
 
-cityAddress = "0x14Fc86cdDf7cFB65E51597b0402C6D6F11f02e90";
-baseAddress = "0x02E664619CCA7aa9382539CcFFffE055aDCC67e2";
-merchantAddress = "0xe74a6231D85Dd06b9E9b9455E841DEEE81A8B1a5";
-landGiveawayAddress = "0x92Cc2e722732ea0822d6D1701023ec388b879b95";
-warAddress = "0x40DAcfF4dAba8F6E45e597Eb6C413471949FD8bB";
-eventAddress = "0x0D872ACd1a7dFA8466f3B23ac20BB5aBC162dfc4";
+cityAddress = "";
+baseAddress = "";
+merchantAddress = "";
+landGiveawayAddress = "";
+warAddress = "";
+eventAddress = "";
 
 async function _address() {
 
